b08b602246ffe4b6a848f5727c4ea321d9a6a2cd
Khanifun Nizar <khanifunnizar@investree.id>
2020-08-14 21:55:33 +0700

    [voen] use env var for config

--- src/main/kotlin/AppConfig.kt
+++ src/main/kotlin/AppConfig.kt
@@ -1,5 +1,5 @@
 object AppConfig {
     const val PLAYER_NAME = "Zeus"
-    const val HOST = "ws://localhost:4000"
-    const val ROOM_ID = "99e3c27b-d69b"
+    val HOST = System.getenv("HOST_URL")
+    val ROOM_ID = System.getenv("ROOM_ID")
 }
